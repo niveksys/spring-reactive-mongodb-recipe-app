624a4f2d99e751423a6bd3f0334feeb51840a300
Kevin Tsang <niveksys@gmail.com>
2021-02-26 16:58:02 +0800

    Added Router Functions for simple RESTful API.

---
+++ src/main/java/com/niveksys/recipeapp/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.niveksys.recipeapp.config;
+
+import com.niveksys.recipeapp.model.Recipe;
+import com.niveksys.recipeapp.service.RecipeService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(GET("/api/recipes"), serverRequest -> ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON).body(recipeService.getRecipes(), Recipe.class));
+
+    }
+}
