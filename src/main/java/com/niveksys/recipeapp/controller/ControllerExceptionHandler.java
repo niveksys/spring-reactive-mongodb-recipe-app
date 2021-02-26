dd1e418c2692f2b7cc6c3c2a3537783cef2f3035
Kevin Tsang <niveksys@gmail.com>
2021-02-26 15:27:56 +0800

    Added WebFlux Controller Advice to handle WebExchangeBindException.

--- src/main/java/com/niveksys/recipeapp/controller/ControllerExceptionHandler.java
+++ src/main/java/com/niveksys/recipeapp/controller/ControllerExceptionHandler.java
@@ -1,18 +1,24 @@
 package com.niveksys.recipeapp.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    // @ResponseStatus(HttpStatus.BAD_REQUEST)
-    // @ExceptionHandler(NumberFormatException.class)
-    // public ModelAndView handleNumberFormatException(Exception ex) {
-    // log.error("Handling Number Format Exception: " + ex.getMessage());
-    // ModelAndView mav = new ModelAndView();
-    // mav.addObject("exception", ex);
-    // mav.setViewName("400error");
-    // return mav;
-    // }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleWebExchangeBindException(Exception ex, Model model) {
+        log.error("Handling Web Exchange Bind Exception: " + ex.getMessage());
+        model.addAttribute("exception", ex);
+        return "400error";
+    }
 
 }
